a80b8ae791140451c7fb939fe92d00850caee0fd
Ivan Trofimov <richard1997dog@gmail.com>
2016-10-19 21:40:46 +0300

    Initial Commit

---
+++ GeoGeo/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  GeoGeo
+//
+//  Created by Иван Трофимов on 19.10.16.
+//  Copyright © 2016 Иван Трофимов. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
