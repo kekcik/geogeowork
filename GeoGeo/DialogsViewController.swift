e06cc854bc4c90a019c95a7c8f07e61400874364
Kirill Averyanov <Kirillzzy@gmail.com>
2017-01-19 17:32:41 +0300

    some fixes in design

---
+++ GeoGeo/DialogsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DialogsViewController.swift
+//  GeoGeo
+//
+//  Created by Kirill Averyanov on 19/01/2017.
+//  Copyright © 2017 Иван Трофимов. All rights reserved.
+//
+
+import UIKit
+
+class DialogsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ GeoGeo/FriendsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FriendsViewController.swift
+//  GeoGeo
+//
+//  Created by Kirill Averyanov on 19/01/2017.
+//  Copyright © 2017 Иван Трофимов. All rights reserved.
+//
+
+import UIKit
+
+class FriendsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+}
